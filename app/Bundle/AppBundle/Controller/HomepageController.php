d07b03700079c7a9fc4b5a6988050724b309c7a0
Maksim Falaleev <max@studio107.ru>
2016-12-07 23:05:56 +0300

    Remove dump

--- app/Bundle/AppBundle/Controller/HomepageController.php
+++ app/Bundle/AppBundle/Controller/HomepageController.php
@@ -9,14 +9,12 @@
 namespace App\Bundle\AppBundle\Controller;
 
 use Mindy\Bundle\MindyBundle\Controller\Controller;
-use Mindy\Orm\OrmFile;
 use Symfony\Component\HttpFoundation\Request;
 
 class HomepageController extends Controller
 {
     public function homepageAction(Request $request)
     {
-        dump(OrmFile::getFilesystem());
         return $this->render('app/homepage.html');
     }
 }
